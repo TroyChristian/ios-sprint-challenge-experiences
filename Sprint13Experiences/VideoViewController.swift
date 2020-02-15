d11e964174eca17e1bebf1c4d974318d492f67f5
Troy Christian <apostasy8@gmail.com>
2020-02-15 17:56:33 -0500

    Camera functionality added

--- Sprint13Experiences/VideoViewController.swift
+++ Sprint13Experiences/VideoViewController.swift
@@ -12,25 +12,163 @@ protocol VideoViewControllerDelegate {
     func addVideoButtonTapped()
 }
 
-class VideoViewController: UIViewController {
-    var experienceController: ExperienceController?
-    var delegate: VideoViewControllerDelegate?
 
+
+import UIKit
+import AVFoundation
+
+class VideoViewController: UIViewController {
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var delegate: VideoViewControllerDelegate?
+    var isRecording: Bool {
+              fileOutput.isRecording
+          }
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var cameraView: CameraPreviewView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Resize camera preview to fill the entire screen
+        cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        setUpCamera()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
     }
-    */
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+        
+        
+        
+    
+    
+    private func setUpCamera() {
+        let camera = bestCamera()
+        
+        
+        // configuration
+        
+        captureSession.beginConfiguration()
+        
+        // Inputs
+        
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Device configured incorrectly")
+        }
+        
+        guard captureSession.canAddInput(cameraInput) else {
+            fatalError("Unable to add camera input")
+            
+        }
+        
+        captureSession.addInput(cameraInput)
+        
+        
+        // 1920x1080p
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        // Microphone
+        
+        
+        
+        
+        // Outputs
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Cannot add file output")
+        }
+        
+        captureSession.addOutput(fileOutput)
+        
+        
+        
+        
+        
+        // commit configuration
+        
+        captureSession.commitConfiguration()
+        
+        cameraView.session = captureSession
+    }
+    private func bestCamera() -> AVCaptureDevice {
+        // front / back
+        // wide angle, ultra wide angle, depth, zoom lens
+        // try ultra wide lens
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        // try wide angle lens
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        fatalError("No cameras available on the device (or you're using a simulator)")
+    }
+
+
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        toggleRecord()
+      
+
+    }
+    
+    func toggleRecord() {
+        if isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+        
+    }
+    /// Creates a new file URL in the documents directory
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    
+    private func updateViews() {
+        recordButton.isSelected = isRecording
+    }
 
 }
+
+extension VideoViewController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        updateViews()
+        
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving recording: \(error)")
+        }
+        print("Url: \(outputFileURL.path)")
+        updateViews()
+        
+    }
+}
+
+
+
+    
+
+   
+
+
